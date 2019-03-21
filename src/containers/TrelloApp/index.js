4535e3a2ec16c3549c5e9e8d7cd4e7c98100987d
Kvrzh <kovr.anton@gmail.com>
2019-03-21 17:21:57 +0200

    commit

--- src/containers/TrelloApp/index.js
+++ src/containers/TrelloApp/index.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
-
+import axios from 'axios';
 import Layout from '@/components/Layout';
 import Boards from '@/containers/Boards';
 import Board from '@/containers/Board';
 
+
 class TrelloApp extends Component {
     send = () => {
-        console.log('qwe');
-    }
+        const width = 600, height = 600;
+        const left = (window.innerWidth / 2) - (width / 2);
+        const top = (window.innerHeight / 2) - (height / 2);
+        const redirectUrl = 'http://localhost:3000/board/1';
+        const url = `https://trello.com/1/authorize?expiration=1day&name=TrelloApp&scope=read&response_type=token&key=321471dffbe1e0c750fa713b81d24145&return_url=${redirectUrl}&callback_method=fragment`
+        window.location.replace(url, '',
+            `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+      scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+      height=${height}, top=${top}, left=${left}`
+        );
+
+    };
+
 
     render() {
-        return(
+        return (
             <Layout
                 isAuth={true}
                 rightButtonAction={() => this.send()}
