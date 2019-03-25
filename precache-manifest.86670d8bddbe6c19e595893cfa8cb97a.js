639eb4a02286524780d85ab1331b9b2bbdbb771c
Kvrzh <kovr.anton@gmail.com>
2019-03-25 12:29:27 +0200

    Updates

---
+++ precache-manifest.86670d8bddbe6c19e595893cfa8cb97a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e86ed5360bd400228f",
+    "url": "/trello-app/static/js/runtime~main.185a5e42.js"
+  },
+  {
+    "revision": "57621e89d9c7ca77a08a",
+    "url": "/trello-app/static/js/main.c3f797eb.chunk.js"
+  },
+  {
+    "revision": "81ed21dd96b7472a03be",
+    "url": "/trello-app/static/js/2.0f87933a.chunk.js"
+  },
+  {
+    "revision": "57621e89d9c7ca77a08a",
+    "url": "/trello-app/static/css/main.b3afa78d.chunk.css"
+  },
+  {
+    "revision": "d568f76f020eb0cd3538facd21560143",
+    "url": "/trello-app/index.html"
+  }
+];
--- src/axios-trello.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const ax = axios.create({
-   baseURL: 'https://api.trello.com/1/'
-});
-
-export default ax;
