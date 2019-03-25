e7a96289f3ac5eed253c2d48ee0a00cc40dd49f9
Kvrzh <kovr.anton@gmail.com>
2019-03-25 12:43:44 +0200

    Updates

---
+++ precache-manifest.6496d0eff77728e27bdf84ac7df27298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e86ed5360bd400228f",
+    "url": "/trello-app/static/js/runtime~main.185a5e42.js"
+  },
+  {
+    "revision": "1275790edf9080030222",
+    "url": "/trello-app/static/js/main.94e9d1e0.chunk.js"
+  },
+  {
+    "revision": "f1b04911853a99707c8b",
+    "url": "/trello-app/static/js/2.002bdcd2.chunk.js"
+  },
+  {
+    "revision": "1275790edf9080030222",
+    "url": "/trello-app/static/css/main.b3afa78d.chunk.css"
+  },
+  {
+    "revision": "22d942dd5d372aaa3faf343bedb24cde",
+    "url": "/trello-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a71057e42b93bf005cbe97e271f43102.js"
+  "/trello-app/precache-manifest.6496d0eff77728e27bdf84ac7df27298.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/trello-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
